2bf8f0362999cc37dcb56af25cd9f13d4e791014
nurlybek <orozovnurlybek@gmail.com>
2025-02-03 23:17:22 +0600

    Созданы контейнеры для трех страниц: Home, Info и Contacts с соответствующим контентом. Импортирован Bootstrap и BrowseRouter

---
+++ src/containers/Contacts/Contacts.tsx
@@ -0,0 +1,10 @@
+
+const Contacts = () => {
+    return (
+            <div className='d-flex flex-column'><span><strong>Phone number</strong>: +34 93 496 36 00</span>
+                <span><strong>Phone attention available:</strong> From Monday to Friday from 9am to 8pm, Saturdays from 10am to 2pm</span>
+                <span><strong>Summer timetable:</strong> (from June 1 to August 15).</span></div>
+    );
+};
+
+export default Contacts;
