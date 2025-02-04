14add777ddf53ffc52c837945b7a6820c951a88b
nurlybek <orozovnurlybek@gmail.com>
2025-02-04 12:45:24 +0600

    Импортировал контейнеры и стилизовал лого

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
 import './App.css'
+import {Route, Routes} from "react-router-dom";
+import Home from "./containers/Home/Home.tsx";
+import Info from "./containers/Info/Info.tsx";
+import Contacts from "./containers/Contacts/Contacts.tsx";
+import Toolbar from "./components/Toolbar/Toolbar.tsx";
 
 const App = () => {
 
+
+    return (
+        <>
+            <header className="mb-5">
+            <Toolbar />
+        </header>
+        <main className='container'>
+            <Routes>
+                <Route path="/"
+                       element={(<Home/>)}></Route>
+                <Route path="/Info"
+                       element={(<Info/>)}></Route>
+                <Route path="/Contacts"
+                       element={(<Contacts/>)}></Route>
+            </Routes>
+        </main>
+
+        </>
+    )
 };
 
 export default App
